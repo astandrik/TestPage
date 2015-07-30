8b4d94f8e403c64ea56382b677e4f0ee9709a199
astandrik <astandrik@gmail.com>
2015-07-30 20:57:42 +0400

    5

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-$('#home').click(function() {$(document).empty();});
 
-});
+$('#home').click(
+    function () {
+        $('#home').fadeOut(400);
+        $('#house').offset($('home').offset());
+        $('#house').removeClass('invisible');
+        $('#house').fadeIn();
+    });
+
+
+$('#house').click(
+    function () {
+        $('#house').fadeOut(400);
+        $('#house').addClass('invisible');
+        $('#home').fadeIn(400);
+    });
